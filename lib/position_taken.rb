0ca92eac5b401492ac17e0e769c4af1b96c2162e
Lucas Souza <souza.lucas01@gmail.com>
2020-03-11 02:27:45 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -4,7 +4,8 @@ def position_taken?(board, index)
 
     if board[index] == " " || "" || nil
       false
-    elsif board[index] == "X" || "O"
+    else board[index] == "X" || "O"
+      true
     end
 
 end
