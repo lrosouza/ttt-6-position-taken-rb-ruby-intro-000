7314a32fc886e074465cc310d1667b2c14f738e8
Lucas Souza <souza.lucas01@gmail.com>
2020-03-11 01:50:47 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,9 +1,13 @@
 # code your #position_taken? method here!
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def position_taken?(board, index)
+
     if board[index] == " " || "" || nil
       false
+    elsif board[index] != " " || "" || nil
+      true
     end
+
     if board[index] == "X" || "O"
       true
     end
